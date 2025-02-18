feb537786e06d07360e8a8c9afde300a0c6c80df
C3rb3rus2050 <135362416+C3rb3rus2050@users.noreply.github.com>
2025-02-18 12:50:50 +0100

    Update climate.py

--- custom_components/cometblue/climate.py
+++ custom_components/cometblue/climate.py
@@ -16,36 +16,24 @@ climate cometblue:
 
 """
 import logging
-from datetime import timedelta
-from datetime import datetime
+from datetime import timedelta, datetime
 import voluptuous as vol
 
-from homeassistant.components.climate import ClimateEntity, PLATFORM_SCHEMA
+from homeassistant.components.climate import ClimateEntity
 from homeassistant.components.climate.const import (
-    ATTR_HVAC_MODE,
-    DOMAIN,
-    ClimateEntityFeature,
-    HVACMode,
+    ATTR_HVAC_MODE, DOMAIN, ClimateEntityFeature, HVACMode,
 )
 from homeassistant.const import (
-    CONF_MAC,
-    CONF_PIN,
-    CONF_DEVICES,
-    ATTR_TEMPERATURE,
-    ATTR_BATTERY_LEVEL,
-    ATTR_LOCKED,
-    PRECISION_HALVES,
-    UnitOfTemperature,
+    CONF_MAC, CONF_PIN, CONF_DEVICES, ATTR_TEMPERATURE, ATTR_BATTERY_LEVEL,
+    ATTR_LOCKED, PRECISION_HALVES, UnitOfTemperature,
 )
-
 import homeassistant.helpers.config_validation as cv
-
 from homeassistant.components import bluetooth
-from homeassistant.core import HomeAssistant, async_get_hass
-
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
 
 _LOGGER = logging.getLogger(__name__)
-_LOGGER.setLevel(10)
+_LOGGER.setLevel(logging.DEBUG)
 
 MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=300)
 
@@ -62,35 +50,28 @@ DEVICE_SCHEMA = vol.Schema({
     vol.Optional(CONF_FAKE_MANUAL, default=False): cv.boolean,
 })
 
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_DEVICES):
-        vol.Schema({cv.string: DEVICE_SCHEMA}),
+PLATFORM_SCHEMA = vol.Schema({
+    vol.Required(CONF_DEVICES): vol.Schema({cv.string: DEVICE_SCHEMA}),
 })
 
 SUPPORT_FLAGS = ClimateEntityFeature.TARGET_TEMPERATURE
 
-
-async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities):
     devices = []
-
+    config = entry.data
     for name, device_cfg in config[CONF_DEVICES].items():
         dev = CometBlueThermostat(str(device_cfg[CONF_MAC]), name, int(device_cfg[CONF_PIN]))
         devices.append(dev)
         if device_cfg[CONF_FAKE_MANUAL]:
             dev.fake_manual_mode = True
-
     async_add_entities(devices)
 
 def device_getter(address):
-    hass = async_get_hass()
-    return bluetooth.async_ble_device_from_address(hass, address, connectable=True)
+    return bluetooth.async_ble_device_from_address(async_get_hass(), address, connectable=True)
 
 class CometBlueThermostat(ClimateEntity):
-    """Representation of a CometBlue thermostat."""
-
     def __init__(self, _mac, _name, _pin=None):
         from cometblue_lite import CometBlue
-        """Initialize the thermostat."""
         self._mac = _mac
         self._name = _name
         self._pin = _pin
@@ -100,89 +81,57 @@ class CometBlueThermostat(ClimateEntity):
 
     @property
     def unique_id(self):
-        """Return unique ID for this device."""
         return self._mac
-    
+
     @property
     def available(self) -> bool:
-        """Return if thermostat is available."""
         return self._thermostat.available
 
     @property
-    def device_info(self):
-        """Return device info."""
-        versions = [self._thermostat.firmware_rev, self._thermostat.software_rev]
-        device_info = {
-            "identifiers": {(DOMAIN, self.unique_id)},
-            "name": self.name,
-            "manufacturer": self._thermostat.manufacturer,
-            "model": self._thermostat.model,
-            "sw_version": ", ".join([rev for rev in versions if rev]),
-        }
-
-        return {k: v for k, v in device_info.items() if v}
-
-    @property
     def supported_features(self):
-        """Return the list of supported features."""
         return SUPPORT_FLAGS
 
     @property
     def name(self):
-        """Return the name of the device."""
         return self._name
 
     @property
     def temperature_unit(self):
-        """Return the unit of measurement that is used."""
         return UnitOfTemperature.CELSIUS
 
     @property
     def precision(self):
-        """Return cometblue's precision 0.5."""
         return PRECISION_HALVES
 
     @property
     def current_temperature(self):
-        """Return current temperature"""
         return self._thermostat.current_temperature
 
     @property
     def target_temperature(self):
-        """Return the temperature we try to reach."""
         return self._thermostat.target_temperature
 
     async def async_set_temperature(self, **kwargs):
-        """Set new target temperature."""
-        if ATTR_HVAC_MODE in kwargs:
-            hvac_mode = kwargs.get(ATTR_HVAC_MODE)
-            self.set_hvac_mode(hvac_mode)
-
         if ATTR_TEMPERATURE in kwargs:
             temperature = kwargs.get(ATTR_TEMPERATURE)
-            _LOGGER.debug("Temperature to set: {}".format(temperature))
             self._thermostat.target_temperature = temperature
             if self.fake_manual_mode:
-                 self._thermostat.target_temperature_high = temperature
-                 self._thermostat.target_temperature_low = temperature
+                self._thermostat.target_temperature_high = temperature
+                self._thermostat.target_temperature_low = temperature
 
     @property
     def min_temp(self):
-        """Return the minimum temperature."""
         return 8.0
 
     @property
     def max_temp(self):
-        """Return the maximum temperature."""
         return 28.0
 
     @property
     def hvac_mode(self):
         if self._thermostat.is_off:
             return HVACMode.OFF
-        elif self._thermostat.manual_mode:
-            return HVACMode.HEAT
-        elif self.fake_manual_mode:
+        elif self._thermostat.manual_mode or self.fake_manual_mode:
             return HVACMode.HEAT
         else:
             return HVACMode.AUTO
@@ -190,11 +139,8 @@ class CometBlueThermostat(ClimateEntity):
     async def async_set_hvac_mode(self, hvac_mode):
         if hvac_mode == self.hvac_mode:
             return
-
-        _LOGGER.debug("HVAC_MODE to set: {}".format(hvac_mode))
         if self.hvac_mode == HVACMode.OFF:
             self._thermostat.is_off = False
-
         if hvac_mode == HVACMode.AUTO:
             self._thermostat.manual_mode = False
         elif hvac_mode == HVACMode.HEAT:
@@ -206,15 +152,10 @@ class CometBlueThermostat(ClimateEntity):
     def hvac_modes(self):
         if self.fake_manual_mode:
             return (HVACMode.HEAT,)
-        elif self._thermostat.firmware_rev == "GEN34BLE":
-            # GENIUS BLE 100 does not support manual mode
-            return (HVACMode.AUTO,)
-        else:
-            return HVACMode.HEAT, HVACMode.AUTO, HVACMode.OFF
+        return (HVACMode.HEAT, HVACMode.AUTO, HVACMode.OFF)
 
     @property
     def extra_state_attributes(self):
-        """Return the device specific state attributes."""
         return {
             ATTR_BATTERY_LEVEL: self._thermostat.battery_level,
             ATTR_BATTERY_LOW: self._thermostat.low_battery,
@@ -223,19 +164,13 @@ class CometBlueThermostat(ClimateEntity):
             ATTR_STATUS: self._thermostat.status,
             ATTR_WINDOW_OPEN: self._thermostat.window_open,
             "model_type": self._thermostat.firmware_rev,
-            "target_high": self._thermostat.target_temperature_high,
-            "target_low": self._thermostat.target_temperature_low,
         }
 
     async def async_update(self):
-        """Update the data from the thermostat."""
         now = datetime.now()
-        if ( 
-            self._thermostat.should_update() or
-            (self._lastupdate and self._lastupdate + MIN_TIME_BETWEEN_UPDATES < now)
-        ):
+        if self._thermostat.should_update() or (self._lastupdate + MIN_TIME_BETWEEN_UPDATES < now):
             try:
                 await self._thermostat.update()
                 self._lastupdate = datetime.now()
             except Exception as ex:
-                _LOGGER.warning("Updating the state for {} failed: {}".format(self._mac, ex))
+                _LOGGER.warning(f"Updating state for {self._mac} failed: {ex}")
